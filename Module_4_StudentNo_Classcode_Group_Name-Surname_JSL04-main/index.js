23a2c602319529f066a2b1598a233e378e714525
OnkarabileMasoko <156708064+OnkarabileMasoko@users.noreply.github.com>
2024-03-22 14:01:25 +0200

    challenge 3 completed

--- Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -24,8 +24,13 @@ console.log(stockCheck);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
 
+if (heartRate < 100) {
+    console.log("Boost needed!");
+  } else {
+    console.log("Energy levels are high!");
+  }
+  
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
